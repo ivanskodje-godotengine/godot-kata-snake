35547384122771f673c8df0bb2a4537d79b66ef1
Ivan Skodje <ivanskodje@protonmail.com>
2021-05-22 22:18:22 +0200

    Clean slate on master, ready for further kata attempts

--- data/global/colors.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-const GREEN_BRIGHTEST = Color("9bbc0f")
-const GREEN_BRIGHT = Color("8bac0f")
-const GREEN_DARK = Color("306230")
-const GREEN_DARKEST = Color("0f380f")
--- data/global/window.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-const PIXEL_SIZE = 4
-const WIDTH = 160
-const HEIGHT = 144
-const SIZE = Vector2(WIDTH, HEIGHT)
